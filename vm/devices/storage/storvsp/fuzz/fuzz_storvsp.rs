ae6fbcec40f5096a0508af8f51c97c1597c54d3b
Matt LaFayette <mattkur@microsoft.com>
2024-12-23 21:11:14 -0800

    wip: does not compile

--- vm/devices/storage/storvsp/fuzz/fuzz_storvsp.rs
+++ vm/devices/storage/storvsp/fuzz/fuzz_storvsp.rs
@@ -19,37 +19,50 @@ use vmbus_async::queue::Queue;
 use vmbus_channel::connected_async_channels;
 //use vmcore::vm_task::{SingleDriverBackend, VmTaskDriverSource};
 use std::sync::mpsc::channel;
+use vmbus_async::queue::OutgoingPacketType;
 use xtask_fuzz::fuzz_eprintln;
 use zerocopy::AsBytes;
-
-// // Anything consumed by EmulatedDeviceFuzzer needs to be static because of DeviceBacking trait.
-// pub static RAW_DATA: Mutex<Vec<u8>> = Mutex::new(Vec::new());
-
-// /// Returns an arbitrary data of type T or a NotEnoughData error. Generic type must
-// /// implement Arbitrary (for any lifetime 'a) and the Sized traits.
-// pub fn arbitrary_data<T>() -> Result<T, arbitrary::Error>
-// where
-//     for<'a> T: Arbitrary<'a> + Sized,
-// {
-//     let mut raw_data = RAW_DATA.lock();
-//     let input = raw_data.split_off(0); // Take all raw_data
-//     let mut u = Unstructured::new(&input);
-
-//     if u.is_empty() {
-//         return Err(arbitrary::Error::NotEnoughData);
-//     }
-
-//     // If bytes needed is more than remaining bytes it will pad with 0s.
-//     let arbitrary_type: T = u.arbitrary()?;
-
-//     let x = u.take_rest().to_vec();
-//     *raw_data = x;
-//     Ok(arbitrary_type)
-// }
 
 #[derive(Arbitrary, Debug)]
 struct StaticFuzzConfig {
     foo: u32,
+}
+
+#[derive(Arbitrary)]
+pub enum StovspFuzzAction {
+    SendDataPacket,
+    SendRawPacket,
+}
+
+#[derive(Arbitrary)]
+enum MyOutgoingPacketType {
+    InBandNoCompletion,
+    InBandWithCompletion,
+    Completion,
+    // TODO: Support GpaDirectPacket
+}
+
+fn arbitrary_outgoing_packet<'_, '_>(
+    u: &'_ mut Unstructured<'_>,
+) -> Result<vmbus_async::queue::OutgoingPacket<'_, '_>, arbitrary::Error> {
+    let packet_type = MyOutgoingPacketType::arbitrary(u)?;
+    match packet_type {
+        MyOutgoingPacketType::InBandNoCompletion => Ok(vmbus_async::queue::OutgoingPacket {
+            transaction_id: u.arbitrary::<u64>()?,
+            packet_type: OutgoingPacketType::InBandNoCompletion,
+            payload: &[&[0u8; 0]], // TODO: [use-arbitrary-input]
+        }),
+        MyOutgoingPacketType::InBandWithCompletion => Ok(vmbus_async::queue::OutgoingPacket {
+            transaction_id: u.arbitrary::<u64>()?,
+            packet_type: OutgoingPacketType::InBandWithCompletion,
+            payload: &[&[0u8; 0]], // TODO: [use-arbitrary-input]
+        }),
+        MyOutgoingPacketType::Completion => Ok(vmbus_async::queue::OutgoingPacket {
+            transaction_id: u.arbitrary::<u64>()?,
+            packet_type: OutgoingPacketType::Completion,
+            payload: &[&[0u8; 0]], // TODO: [use-arbitrary-input]
+        }),
+    }
 }
 
 fn do_fuzz(u: &mut Unstructured<'_>) -> Result<(), anyhow::Error> {
@@ -57,15 +70,14 @@ fn do_fuzz(u: &mut Unstructured<'_>) -> Result<(), anyhow::Error> {
 
     DefaultPool::run_with(|driver| async move {
         // set up the channels and worker
-        //let channel_count = u.int_in_range(16..=17)?; // TODO: figure out why this needs 16K  space, it seems.
-        let channel_count = 16;
+        let channel_count = 16; // TODO: [use-arbitrary-input] (figure out why this needs 16K  space, it seems.)
         let (host, guest) = connected_async_channels(channel_count * 1024);
         let guest_queue = Queue::new(guest).unwrap();
 
-        let guest_mem_pages = u.int_in_range(1..=64)?;
+        let guest_mem_pages = u.int_in_range(1..=256)?;
         let test_guest_mem = GuestMemory::allocate(guest_mem_pages * 4096); // TODO: [use-arbitrary-input]
         let controller = ScsiController::new();
-        let disk_len_sectors = u.int_in_range(1..=40960)?;
+        let disk_len_sectors = u.int_in_range(1..=1048576)?; // up to 512mb in 512 byte sectors
         let disk = scsidisk::SimpleScsiDisk::new(
             disklayer_ram::ram_disk(disk_len_sectors * 512, false).unwrap(), // TODO: [use-arbitrary-input]
             Default::default(),
@@ -88,12 +100,19 @@ fn do_fuzz(u: &mut Unstructured<'_>) -> Result<(), anyhow::Error> {
             transaction_id: 0,
         };
 
-        // TODO: Decide whether to do protocol nogotiation or not based on arbitrary.
-        guest.perform_protocol_negotiation().await;
-
         while !u.is_empty() {
-            if let Ok(packet) = storvsp::protocol::Packet::arbitrary(u) {
-                let _ = guest.send_data_packet_sync(&[packet.as_bytes()]).await;
+            let action = u.arbitrary::<StovspFuzzAction>()?;
+            match action {
+                StovspFuzzAction::SendDataPacket => {
+                    if let Ok(packet) = storvsp::protocol::Packet::arbitrary(u) {
+                        let _ = guest.send_data_packet_sync(&[packet.as_bytes()]).await;
+                    }
+                }
+                StovspFuzzAction::SendRawPacket => {
+                    if let Ok(packet) = arbitrary_outgoing_packet(u) {
+                        guest.queue.split().1.write(packet).await?;
+                    }
+                }
             }
         }
 
@@ -105,11 +124,6 @@ fn do_fuzz(u: &mut Unstructured<'_>) -> Result<(), anyhow::Error> {
 
 fuzz_target!(|input: &[u8]| -> libfuzzer_sys::Corpus {
     xtask_fuzz::init_tracing_if_repro();
-
-    // {
-    //     let mut raw_data = RAW_DATA.lock();
-    //     *raw_data = input;
-    // }
 
     if do_fuzz(&mut Unstructured::new(input)).is_err() {
         libfuzzer_sys::Corpus::Reject
