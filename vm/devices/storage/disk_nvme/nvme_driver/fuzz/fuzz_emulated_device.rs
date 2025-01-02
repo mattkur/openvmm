09d13555ef75f2f727f31b41dfa339be8e20255c
Matt LaFayette <mattkur@microsoft.com>
2025-01-02 11:03:38 -0800

    wip

--- vm/devices/storage/disk_nvme/nvme_driver/fuzz/fuzz_emulated_device.rs
+++ vm/devices/storage/disk_nvme/nvme_driver/fuzz/fuzz_emulated_device.rs
@@ -2,6 +2,9 @@
 // Licensed under the MIT License.
 
 //! A shim layer to fuzz responses from an emulated device.
+use std::collections::HashMap;
+use std::iter::Map;
+
 use crate::arbitrary_data;
 
 use chipset_device::mmio::MmioIntercept;
@@ -15,11 +18,18 @@ use user_driver::emulated::EmulatedDmaAllocator;
 use user_driver::emulated::Mapping;
 use user_driver::interrupt::DeviceInterrupt;
 use user_driver::DeviceBacking;
+use user_driver::DeviceRegisterIo;
 
 /// An EmulatedDevice fuzzer that requires a working EmulatedDevice backend.
 #[derive(Inspect)]
-pub struct FuzzEmulatedDevice<T: InspectMut> {
+pub struct FuzzEmulatedDevice<T: PciConfigSpace + MmioIntercept + InspectMut> {
     device: EmulatedDevice<T>,
+    #[inspect(skip)]
+    bars: HashMap<u8, <FuzzEmulatedDevice<T> as DeviceBacking>::Registers>,
+}
+
+pub struct FuzzMapping<T> {
+    device_bar: Mapping<T>,
 }
 
 impl<T: PciConfigSpace + MmioIntercept + InspectMut> FuzzEmulatedDevice<T> {
@@ -27,7 +37,38 @@ impl<T: PciConfigSpace + MmioIntercept + InspectMut> FuzzEmulatedDevice<T> {
     pub fn new(device: T, msi_set: MsiInterruptSet, shared_mem: DeviceSharedMemory) -> Self {
         Self {
             device: EmulatedDevice::new(device, msi_set, shared_mem),
+            bars: HashMap::new(),
         }
+    }
+}
+
+impl<T: PciConfigSpace + MmioIntercept + InspectMut> DeviceRegisterIo for FuzzMapping<T> {
+    fn read_u32(&self, offset: usize) -> u32 {
+        if let Ok(true) = arbitrary_data::<bool>() {
+            if let Ok(data) = arbitrary_data::<u32>() {
+                return data;
+            }
+        }
+
+        self.device_bar.read_u32(offset)
+    }
+
+    fn read_u64(&self, offset: usize) -> u64 {
+        if let Ok(true) = arbitrary_data::<bool>() {
+            if let Ok(data) = arbitrary_data::<u64>() {
+                return data;
+            }
+        }
+
+        self.device_bar.read_u64(offset)
+    }
+
+    fn write_u32(&self, offset: usize, data: u32) {
+        self.device_bar.write_u32(offset, data)
+    }
+
+    fn write_u64(&self, offset: usize, data: u64) {
+        self.device_bar.write_u64(offset, data)
     }
 }
 
@@ -41,12 +82,18 @@ impl<T: 'static + Send + InspectMut + MmioIntercept> DeviceBacking for FuzzEmula
     }
 
     fn map_bar(&mut self, n: u8) -> anyhow::Result<Self::Registers> {
-        self.device.map_bar(n)
+        let device_bar = self.device.map_bar(n)?;
+
+        let fuzz_mapping = FuzzMapping { device_bar };
+        self.bars.insert(n, fuzz_mapping);
+
+        Ok(fuzz_mapping)
     }
 
     fn host_allocator(&self) -> Self::DmaAllocator {
         self.device.host_allocator()
     }
+
 
     /// Arbitrarily decide to passthrough or return arbitrary value.
     fn max_interrupt_count(&self) -> u32 {
